5dd371e9400870a44b1ae1f4f90ec0b04e95bd98
Daniel Hackiewicz <dhackiewicz@gmail.com>
2025-03-11 16:31:38 +0100

    ext_localconf

--- src/packages/site_package/ext_localconf.php
+++ src/packages/site_package/ext_localconf.php
@@ -3,9 +3,3 @@ defined('TYPO3');
 
 // Add default RTE configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['site_package'] = 'EXT:site_package/Configuration/RTE/Default.yaml';
-
-//$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\GeorgRinger\News\Domain\Model\News::class] = [
-//    'className' => \StudioThomPfister\SitePackage\Domain\Model\News::class
-//];
-//$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News']['site_package'] = 'site_package';
-//$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'] = \StudioThomPfister\SitePackage\Domain\Model\News::class;
