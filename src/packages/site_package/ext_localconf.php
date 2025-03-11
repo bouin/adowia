6c86e82f748a5000495c848b9237e9e67d11d318
Daniel Hackiewicz <dhackiewicz@gmail.com>
2025-03-11 14:57:24 +0100

    second changes

--- src/packages/site_package/ext_localconf.php
+++ src/packages/site_package/ext_localconf.php
@@ -7,5 +7,5 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['site_package'] = 'EXT:site_packag
 //$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\GeorgRinger\News\Domain\Model\News::class] = [
 //    'className' => \StudioThomPfister\SitePackage\Domain\Model\News::class
 //];
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News']['site_package'] = 'site_package';
+//$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News']['site_package'] = 'site_package';
 //$GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'] = \StudioThomPfister\SitePackage\Domain\Model\News::class;
